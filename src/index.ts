51fe2349050860fed09a7a9bd1b089c0d9ef35d4
Tai Groot <tai@taigrr.com>
2025-01-13 23:12:22 +0000

    final version in booth

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-const message: string = "Hello world!";
-console.log(message);
+function FizzBuzz(value: number):string |number {
+  if (value %15 ==0){
+      return "FizzBuzz"
+    } else if (value % 5 ==0){
+      return "Buzz"
+    } else if (value%3 ==0){
+      return "Fizz"
+    } else{
+      return value
+    }
+}
+for (let i=0; i < 30; i++){
+  let output: string | number = FizzBuzz(i)
+  console.log(output)
+}
