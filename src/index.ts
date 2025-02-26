84200f2040a9d686e59f878dcbffb294868b94f8
Tai Groot <tai@taigrr.com>
2025-02-25 23:01:12 -0800

    prettier

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-function FizzBuzz(value: number):string |number {
-  if (value %15 ==0){
-      return "FizzBuzz"
-    } else if (value % 5 ==0){
-      return "Buzz"
-    } else if (value%3 ==0){
-      return "Fizz"
-    } else{
-      return value
-    }
+function FizzBuzz(value: number): string | number {
+  if (value % 15 == 0) {
+    return "FizzBuzz";
+  } else if (value % 5 == 0) {
+    return "Buzz";
+  } else if (value % 3 == 0) {
+    return "Fizz";
+  } else {
+    return value;
+  }
 }
-for (let i=0; i < 30; i++){
-  let output: string | number = FizzBuzz(i)
-  console.log(output)
+for (let i = 0; i < 30; i++) {
+  let output: string | number = FizzBuzz(i);
+  console.log(output);
 }
